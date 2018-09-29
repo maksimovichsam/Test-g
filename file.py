4adbfc21c9ca6c4ec715d092e9cb7c7409799ead
Samuel Maksimovich <samaksimovich@ucdavis.edu>
2018-09-29 12:32:06 -0700

    Removed testing code

--- file.py
+++ file.py
@@ -2,5 +2,3 @@ size = int(input("Enter size of pyramid: "))
 for n in range(size, 0, -1):
     line = (' ' * (n - 1)) + 'x' + ('x' * 2 * (size - n))
     print(line)
-print(":)")
-print("this is from testbranch")
